11b7deff3cc66db21789a4fde7f3c6fe75a51a76
katerynaLevina <rg9a.klew@gmail.com>
2026-03-15 15:17:03 +0200

    +мютекс

--- main.cpp
+++ main.cpp
@@ -33,11 +33,49 @@ void runLinear(int* data, int N) {
     cout << "Execution time: "  << duration_sec.count() << " s\n\n";
 }
 
+void runMutex(int* data, int N, int numThreads) {
+    long long global_count = 0;
+    int global_min = numeric_limits<int>::max();
+    mutex mtx;
 
+    thread* threads = new thread[numThreads];
+    int chunkSize = N / numThreads;
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+    for (int t = 0; t < numThreads; ++t) {
+        int start_idx = t * chunkSize;
+        int end_idx = (t == numThreads - 1) ? N : (t + 1) * chunkSize;
+
+        threads[t] = thread([&, start_idx, end_idx]() {
+            for (int i = start_idx; i < end_idx; ++i) {
+
+                if (data[i] % DIVISOR == 0) {
+                    lock_guard<mutex> lock(mtx);
+                    global_count++;
+                    if (data[i] < global_min) global_min = data[i];
+                }
+            }
+        });
+    }
+
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i].join();
+    }
+
+    auto end_time = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration_sec = end_time - start_time;
+
+    cout << "--- Mutex (" << numThreads << " threads) ---\n";
+    cout << "Result: Count = " << global_count << " | Min = " << global_min << "\n";
+    cout << "Execution time: "  << duration_sec.count() << " s\n\n";
+
+    delete[] threads;
+}
 
 int main() {
     int N = 10000000;
-
+    int numThreads = 8;
 
     cout << "Initializing array of size " << N << "...\n\n";
     int* data = new int[N];
@@ -49,7 +87,7 @@ int main() {
 
 
     runLinear(data, N);
-
+    runMutex(data, N, numThreads);
 
 
     delete[] data;
